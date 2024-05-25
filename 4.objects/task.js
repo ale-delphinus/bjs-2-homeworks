31a45a199dc2d44ea1387d3cbc87ffb914746db4
Evteeva Ksenia <slepzova.ksenia@mail.ru>
2024-05-26 01:39:02 +0300

    Objects done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+new Student("Петр Иванов");
+new Student("Иван Сидоров");
+new Student("Мария Петрова");
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+    let marksProperty = this.hasOwnProperty("marks");
+    if(marksProperty === true) {
+        this.marks.push(...marks);
+    }
   
 }
 
 Student.prototype.getAverage = function () {
-  
+    let marksProperty = this.hasOwnProperty("marks");
+    if(marksProperty === false) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, item)=> 
+            acc+=item/this.marks.length, 0);
+        } 
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
